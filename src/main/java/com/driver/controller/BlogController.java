8166980347cee5b8088c58377f2c23fbb9c98f50
mudasirabdullah52 <mudasir.ahmad.mca.2019@miet.ac.in>
2023-01-14 22:20:50 +0530

    check

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -13,33 +13,34 @@ import java.util.List;
 @RequestMapping("/blogs")
 public class BlogController {
 
-    @GetMapping
+    @Autowired
+    BlogService blogService;
+    @GetMapping("get_all")
     public ResponseEntity<Integer> getAllBlogs() {
-        int countOfBlogs = 0;
+        List<Blog> blogList = blogService.showBlogs();
+        int countOfBlogs = blogList.size();
+
         return new ResponseEntity<>(countOfBlogs, HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity createBlog(@RequestParam Integer userId ,
-                                           @RequestParam String title,
-                                           @RequestParam String content) {
+                                     @RequestParam String title,
+                                     @RequestParam String content) {
 
+        blogService.createAndReturnBlog(userId,title,content);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{blogId}/add-image")
     public ResponseEntity<String> addImage(@PathVariable int blogId, @RequestParam String description, @RequestParam String dimensions) {
-
-            return new ResponseEntity<>("Added image successfully", HttpStatus.OK);
+        blogService.addImage(blogId,description,dimensions);
+        return new ResponseEntity<>("Added image successfully", HttpStatus.OK);
     }
 
     @DeleteMapping("/{blogId}")
     public ResponseEntity<Void> deleteBlog(@PathVariable int blogId) {
-
+        blogService.deleteBlog(blogId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
-
-
-
-
